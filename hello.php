30a89aad7b7e95d90bc6dd501e2b6eda26903d59
Bill Harper <biharper@cisco.com>
2015-12-22 10:15:00 -0800

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello world, Cloud Foundry Testing on MetaPod ... </title>
+    </head>
+    <body>
+          <p>Hello MetaPod world<?php echo " version 1"; ?></p>
+    </body>
+</html>
